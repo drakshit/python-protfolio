a630262db6ec85579a565b5290673697c0288a0d
Debasish Rakshit <debasish.rakshit@volvocars.com>
2025-05-04 15:29:35 +0200

    Create column and abouts me section

---
+++ main.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image('images/photo.jpg', width=400)
+with col2:
+    st.title("About Debasish!")
+    about_me = """
+        Dynamic Full-Stack Developer with 16 years of experience delivering innovative web
+        solutions and significantly enhancing user engagement. Expertise encompasses both
+        front-end and back-end technologies, with a strong focus on React JS, Next JS,
+        and Node JS, facilitating the creation of scalable and high-performance applications.
+        Proficiency in Agile methodologies and CI/CD practices ensures timely project delivery
+        and efficient processes. Strong analytical skills combined with a passion for leveraging
+        cloud technologies contribute to the development of robust and maintainable systems.
+        Committed to continuous improvement and fostering collaboration within diverse team
+        environments.
+    """
+    st.info(about_me)
+note = """
+Below you'll find some apps which I have built in Python, feel free to connect me!
+"""
+st.write(note)
